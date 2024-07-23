26393bdebcb92d94402cd9c483378839296307e9
Alex Kissi Jr <info@alexkissijr.com>
2024-07-22 21:26:47 -0700

    working with audoi

--- src/App.js
+++ src/App.js
@@ -9,25 +9,52 @@ function App() {
   // but it could not work as expected
   // until the codebaby is fully initialized
   // you could use by <AvatarProvider on'EventName'={...} />
+
+  const handleAvatarClick = useCallback(async () => {
+    console.log('Avatar touched');
+    // Resume AudioContext if it's suspended
+    const audioContext = (aProviderRef.current)?.audioContext;
+    if (audioContext && audioContext.state === 'suspended') {
+      await audioContext.resume();
+      console.log('AudioContext resumed');
+    }
+
+    if (aProviderRef.current) {
+      aProviderRef.current.trigger('na-microphoneStarted.vidbaby', {});
+      aProviderRef.current.trigger('na-microphoneToggle.vidbaby', {});
+      console.log('Microphone enabled');
+    }
+  }, []);
+
   useEffect(() => {
     if(!isInitialized) return;
     const onResponse = (e, responseData) => {
       console.log('ResponseMessage', responseData);
     };
     const aProvider = aProviderRef.current;
-    if(aProvider) {
-      aProvider.on('response', onResponse);
-      return () => {
-        aProvider.off('response', onResponse);
-      }
+
+    const button = document.getElementById('avatar-button');
+    if (button) {
+      button.addEventListener('touchstart', handleAvatarClick);
     }
-  }, [aProviderRef, isInitialized]);
+    if(aProvider){
+      aProvider.on('response', onResponse);
+    }
+    return () => {
+      if (button) {
+        button.removeEventListener('touchstart', handleAvatarClick);
+      }
+      aProvider.off('response', onResponse);
+
+    };
+   
+  }, [aProviderRef, isInitialized, handleAvatarClick]);
 
   // you can use ref to trigger the events too
-  const handleHello = useCallback(() => {
+  const handleHello = useCallback(() =>  {
     if(aProviderRef.current) {
       console.log('Triggering playResponse')
-      //aProviderRef.current.trigger('playResponse', { answer: 'Hey! Wanna develop an app?' });
+      //aProviderRef.current.trigger('playResponse', { answer: 'Hey! Just tap the screen to get started?' });
       aProviderRef.current.trigger('ask', { text: 'Hello' });
     }
   }, []);
@@ -45,6 +72,8 @@ function App() {
         handleHello();
       }}
       id="react-avatar">
+        <div id="codebaby-container" className="w-full h-full"></div>
+        <button id="avatar-button"></button>
     </AvatarProvider>
   );
 }
